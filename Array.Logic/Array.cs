05f9c8cf477f4e2b524137529d68ec5c6e162fd1
denaranjol <denarnajol@hotmail.com>
2024-02-07 21:24:42 -0500

    Add project files.

---
+++ Array.Logic/Array.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Logic
+{
+    public class ArrayNew
+    {
+        //atributos
+        private int[] array;
+        private int top;
+
+        //constructor
+        public ArrayNew(int num)
+        {
+            array = new int[num];
+            N = num;
+            top = 0;
+        }
+
+        //propiedades
+        public int N { get; }
+
+        public bool IsFull => top == N;
+        public bool IsEmpty => top == 0;
+
+        public void Fill(int min, int max)
+        {
+            Random randomico = new Random();
+
+            for (int i = 0; i < N; i++) 
+            {
+                array[i] = randomico.Next(min, max);
+            }
+            top = N;
+        }
+
+        public override string ToString()
+        {
+            var output = "";
+            if (IsEmpty)
+            {
+                return "Array Empty";
+            }
+
+            var count = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if(count == 10)
+                {
+                    output += "\n";
+                    count = 0;
+                }
+                output += $"{array[i]}\t";
+                count++;
+            }
+            return output;
+        }
+    }
+}
---
+++ Array.UX/Program.cs
@@ -0,0 +1,7 @@
+﻿using Array.Logic;
+using static System.Console;
+
+var hola = new ArrayNew(10);
+hola.Fill(10,40);
+
+WriteLine(hola);
